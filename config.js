919b17d1f89b1afcb8e6fae8259fbdb76a986b79
kim-5500 <jk004443@gmail.com>
2026-09-08 20:05:27 +0300

    Update UNIKENYA_API_BASE to new backend URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Set this to your deployed HTTPS backend URL before release, e.g. https://api.example.com
-window.UNIKENYA_API_BASE = 'https://unikenya.onrender.com';
+window.UNIKENYA_API_BASE = 'https://unikenya-n7dw.onrender.com';
